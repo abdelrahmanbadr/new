bfa1a2b66ea095f47d8290347c813ec8fdcc08a2
abdelrahmanbadr <abdelrahmanbadr.it@gmail.com>
2017-03-15 01:03:05 -0700

    finsihed

--- app.js
+++ app.js
@@ -13,62 +13,64 @@ var app = express();
 var server = require('http').createServer(app);
 
 var io=require('socket.io').listen(server);
-var Redis = require('ioredis');
-var redis=new Redis();
-
-
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
-
-// // uncomment after placing your favicon in /public
-// //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-// app.use(logger('dev'));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/', index);
-// app.use('/users', users);
 
 
 
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
+//view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
 
-//module.exports = app;
+app.use('/users', users);
+
+app.get('/', function(req, res) {
+
+  res.sendFile( __dirname + "/index.html" );
+});
+app.get('/see', function(req, res) {
+
+  res.sendFile( __dirname + "/preview.html" );
+});
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
 
 server.listen(3000,function(){
   console.log("Server Runing");
 });
 
-//////////////////routes
 
-/////////////////chat
+io.on('connection',function(socket){
 
-redis.subscribe('ionic-laravel');
-///channel must be ionic-laravel
+socket.on('stream',function(image){
+	socket.broadcast.emit('stream',image);
 
-redis.on('message',function(channel,data){
-
-data=JSON.parse(data);
- io.emit(channel, data.data.msg,data.data.room,data.data.name);
-
+});
 });
 
